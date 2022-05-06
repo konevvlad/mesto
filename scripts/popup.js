8e466b2411dcfa09c016e6e8163e1f2813333050
Vlad Konev <ZinaidaSielberthrud@yandex.ru>
2022-05-07 04:38:29 +0500

    vtoraya iteraciya

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,24 +4,22 @@ let popupCloseBtn = document.querySelector('.popup__close-button');
 let inputName = document.querySelector('.popup__profile-title-input')
 let inputJob = document.querySelector('.popup__profile-subtitle-input')
 let profileName = document.getElementById('profile__name');
-let profileJob = document.getElementById('profile__job')
-console.log(profileName.textContent)
-console.log(inputName.textContent)
+let profileJob = document.getElementById('profile__job');
+let formElement = document.querySelector('.popup__save-button');
+
+
+
 popupOpenBtn.addEventListener('click', function() {
     popup.classList.add('popup_active')
-    inputName.setAttribute('value', profileName.textContent)
-    inputJob.setAttribute('value', profileJob.textContent)
+    inputName.value=profileName.textContent
+    inputJob.value=profileJob.textContent
     
-
 }); 
 
 popupCloseBtn.addEventListener('click', function() {
     popup.classList.remove('popup_active')
 })
 
-
-
-let formElement = document.querySelector('.popup__save-button')
 
 formElement.addEventListener('click', function(evt) {
         evt.preventDefault (); 
