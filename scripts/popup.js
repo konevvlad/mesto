4b02f95a3254be30a8a3aeee29c9dae68ffb917a
Vlad Konev <ZinaidaSielberthrud@yandex.ru>
2022-05-07 04:42:10 +0500

    vtoraya iteraciya1.0

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,25 +1,30 @@
+let profileName = document.getElementById('profile__name');
+let profileJob = document.getElementById('profile__job')
+
+let inputName = document.querySelector('.popup__profile-input_title')
+let inputJob = document.querySelector('.popup__profile-input_subtitle')
 let popupOpenBtn = document.querySelector('.profile__edit-button');
+
 let popup = document.querySelector('.popup');
 let popupCloseBtn = document.querySelector('.popup__close-button');
-let inputName = document.querySelector('.popup__profile-title-input')
-let inputJob = document.querySelector('.popup__profile-subtitle-input')
-let profileName = document.getElementById('profile__name');
-let profileJob = document.getElementById('profile__job');
-let formElement = document.querySelector('.popup__save-button');
 
-
-
-popupOpenBtn.addEventListener('click', function() {
-    popup.classList.add('popup_active')
-    inputName.value=profileName.textContent
-    inputJob.value=profileJob.textContent
-    
-}); 
 
 popupCloseBtn.addEventListener('click', function() {
-    popup.classList.remove('popup_active')
+    popup.classList.remove('popup_active');
+    inputName.removeAttribute('value')
+    inputJob.removeAttribute('value')
+
+});
+
+popupOpenBtn.addEventListener('click', function() {
+    inputName.value=profileName.textContent;
+    inputJob.value=profileJob.textContent;
+    popup.classList.add('popup_active');   
+
 })
 
+
+let formElement = document.querySelector('.popup__save-button')
 
 formElement.addEventListener('click', function(evt) {
         evt.preventDefault (); 
